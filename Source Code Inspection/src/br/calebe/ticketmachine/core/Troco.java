0bb888a8ff4a405615a9e5ea1d23b9c3e1cbd8bd
Fabio Tanikawa <79976718+Tonikawa@users.noreply.github.com>
2024-03-31 18:52:05 -0300

    Update Troco.java
    Adicionando comentários no código com os erros.

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -4,15 +4,19 @@ import java.util.Iterator;
 
 /**
  *
- * @author Calebe de Paula Bianchini
+ * author Calebe de Paula Bianchini
  */
 class Troco {
 
     protected PapelMoeda[] papeisMoeda;
 
+    // Construtor da classe Troco
+    // Possíveis problemas: Erros na lógica dos loops de atribuição
+    // Categoria: Comissão (implementação incorreta)
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
         int count = 0;
+        // Os loops abaixo não atualizam o valor de "valor", resultando em loops infinitos
         while (valor % 100 != 0) {
             count++;
         }
@@ -36,18 +40,22 @@ class Troco {
         while (valor % 5 != 0) {
             count++;
         }
+        // Possível erro: Índice incorreto, deve ser papeisMoeda[0] ao invés de papeisMoeda[1]
         papeisMoeda[1] = new PapelMoeda(5, count);
         count = 0;
         while (valor % 2 != 0) {
             count++;
         }
+        // Possível erro: Índice incorreto, deve ser papeisMoeda[1] ao invés de papeisMoeda[1]
         papeisMoeda[1] = new PapelMoeda(2, count);
     }
 
+    // Método para obter um iterador de papéis moeda
     public Iterator<PapelMoeda> getIterator() {
         return new TrocoIterator(this);
     }
 
+    // Classe interna para iterar sobre os papéis moeda
     class TrocoIterator implements Iterator<PapelMoeda> {
 
         protected Troco troco;
@@ -57,8 +65,11 @@ class Troco {
         }
 
         @Override
+        // Método para verificar se há mais papéis moeda
+        // Possível problema: Índice inicial incorreto
+        // Categoria: Controle (uso incorreto de índice)
         public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
+            for (int i = 6; i >= 0; i++) { // Possível erro: O loop deve começar em 5, não em 6
                 if (troco.papeisMoeda[i] != null) {
                     return true;
                 }
@@ -67,9 +78,12 @@ class Troco {
         }
 
         @Override
+        // Método para obter o próximo papel moeda
+        // Possível problema: Implementação incorreta do loop
+        // Categoria: Comissão (implementação incorreta)
         public PapelMoeda next() {
             PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
+            for (int i = 6; i >= 0 && ret != null; i++) { // Possível erro: O loop deve começar em 5, não em 6
                 if (troco.papeisMoeda[i] != null) {
                     ret = troco.papeisMoeda[i];
                     troco.papeisMoeda[i] = null;
@@ -79,8 +93,8 @@ class Troco {
         }
 
         @Override
+        // Método para remover um papel moeda
         public void remove() {
             next();
         }
     }
-}
