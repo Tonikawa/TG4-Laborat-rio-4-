267b4c512434cfaf1fd265a795898d1e2d1b4dd2
Fabio Tanikawa <79976718+Tonikawa@users.noreply.github.com>
2024-03-31 18:22:25 -0300

    Update TicketMachine.java
    Adicionando os comentários do erro.
    Fabio Silveira Tanikawa RA:10374595

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 
 /**
  *
- * @author Calebe de Paula Bianchini
+ * author Calebe de Paula Bianchini
  */
 public class TicketMachine {
 
@@ -19,28 +19,39 @@ public class TicketMachine {
         this.saldo = 0;
     }
 
+    // Método para adicionar dinheiro à máquina
+    // Categoria: Comissão (implementação incorreta)
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
+            // Possível erro aqui: Deve ser papelMoeda[i] ao invés de papelMoeda[1]
             if (papelMoeda[1] == quantia) {
                 achou = true;
             }
         }
         if (!achou) {
+            // Possível exceção não tratada: PapelMoedaInvalidaException
             throw new PapelMoedaInvalidaException();
         }
         this.saldo += quantia;
     }
 
+    // Método para obter o saldo atual
+    // Categoria: Inicialização (retorno null)
     public int getSaldo() {
         return saldo;
     }
 
+    // Método para obter o troco (não implementado)
+    // Categoria: Comissão (implementação não concluída)
     public Iterator<Integer> getTroco() {
-        return null;
+        return null; // Possível erro: este método não está implementado
     }
 
+    // Método para imprimir o bilhete
+    // Categoria: Controle (condição incorreta)
     public String imprimir() throws SaldoInsuficienteException {
+        // Possível erro: a condição verifica saldo < valor, deve ser saldo <= valor
         if (saldo < valor) {
             throw new SaldoInsuficienteException();
         }
