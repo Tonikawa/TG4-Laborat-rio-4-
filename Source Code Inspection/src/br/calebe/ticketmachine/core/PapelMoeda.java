cbb8cda58d2ccd5c26e19306e41c31a7e243fb3f
Fabio Tanikawa <79976718+Tonikawa@users.noreply.github.com>
2024-03-31 18:43:49 -0300

    Update PapelMoeda.java
    Adicionando os comentários com os erros.
    Fabio Silveira Tanikawa RA: 10374595

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -2,22 +2,27 @@ package br.calebe.ticketmachine.core;
 
 /**
  *
- * @author Calebe de Paula Bianchini
+ * author Calebe de Paula Bianchini
  */
 public class PapelMoeda {
 
-    protected int valor;
-    protected int quantidade;
+    protected int valor; // Valor da nota ou moeda
+    protected int quantidade; // Quantidade disponível
 
+    // Construtor da classe PapelMoeda
+    // Possível problema: Ausência de validação de entrada
+    // Categoria: Comissão (implementação incorreta)
     public PapelMoeda(int valor, int quantidade) {
         this.valor = valor;
         this.quantidade = quantidade;
     }
 
+    // Método para obter o valor do papel moeda
     public int getValor() {
         return valor;
     }
 
+    // Método para obter a quantidade disponível do papel moeda
     public int getQuantidade() {
         return quantidade;
     }
